bfa2df37764b18d7b3861d6d4ed0f0c1acbf514e
rmacdonald <rmacdonald@astro.cornell.edu>
2022-03-02 14:13:02 -0500

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name = 'POSEIDON',
-    version = '0.5.1',
+    version = '0.5.2',
     description = 'Exoplanet atmospheric retrieval package',
     long_description = open(os.path.join(
                             os.path.dirname(__file__), 'README.rst')).read(),
