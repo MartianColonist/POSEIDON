6d37f9cdfc21ac1d20633aab51ab287fd702322e
Ryan MacDonald <rmacdonald@astro.cornell.edu>
2022-07-13 22:36:23 -0400

    Test numpy increment

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     packages = ['POSEIDON'],
     include_package_data = True,
     python_requires = '<3.10',
-    install_requires = ['numpy<=1.21',
+    install_requires = ['numpy<=1.22',
                         'scipy',
                         'matplotlib<=3.5.1',
                         'astropy',
