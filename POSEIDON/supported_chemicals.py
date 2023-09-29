b5ded967d0950b1b764abe5aa08eab3939b9d003
Ryan MacDonald <rmacdonald@astro.cornell.edu>
2023-09-29 18:17:08 -0400

    Added new atoms and ions

--- POSEIDON/supported_chemicals.py
+++ POSEIDON/supported_chemicals.py
@@ -13,7 +13,8 @@ supported_species = np.array(['AlH', 'AlO', 'BeH', 'C2H2', 'CH', 'CH4', 'CO',
                               'NO', 'NO2', 'Na', 'NaH', 'O2', 'O3', 'OH', 
                               'PH3', 'PN', 'PO', 'PS', 'Rb', 'SH', 'SO2', 'ScH', 
                               'SiH', 'SiO', 'Ti', 'Ti+', 'TiH', 'TiO', 'VO',
-                              'Mg', 'Mg+', 'Ca', 'Ca+', 'Mn'])
+                              'Mg', 'Mg+', 'Ca', 'Ca+', 'Mn' 'Cr', 'V', 'V+',
+                              'Ba', 'Ba+', 'Al', 'Ni', 'O', 'Sc', 'H'])
 
 # Chemical species supported in the fastchem equilibrium grid
 fastchem_supported_species = np.array(['H2O', 'CO2', 'OH', 'SO', 'C2H2', 
@@ -30,4 +31,4 @@ supported_cia = np.array(['H2-H2', 'H2-He', 'H2-H', 'H2-CH4', 'CO2-H2', 'N2-H2',
                           'O2-CO2', 'O2-N2'])
 
 # Species treated as spectrally inactive (in standard cross section treatment)
-inactive_species = np.array(['H2', 'He', 'H', 'e-', 'H-'])  # H- handled separately
+inactive_species = np.array(['H2', 'He', 'e-', 'H-'])  # H- handled separately
