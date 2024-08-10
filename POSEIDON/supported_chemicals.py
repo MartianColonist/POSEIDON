3020e3726e39f6c88fb4f15beda171d1a25d9b8a
Ryan MacDonald <rmacdonald@astro.cornell.edu>
2024-08-09 22:29:47 -0400

    Update supported chemical species

--- POSEIDON/supported_chemicals.py
+++ POSEIDON/supported_chemicals.py
@@ -6,16 +6,17 @@ Supported chemical species in the current version of POSEIDON.
 import numpy as np
 
 # Chemical species with cross sections included in opacity database
-supported_species = np.array(['AlH', 'AlO', 'BeH', 'C2H2', 'CH', 'CH4', 'CO', 
-                              'CO2', 'CaH', 'CaO', 'CrH', 'Cs', 'Fe', 'Fe+', 
-                              'FeH', 'H2', 'H2O', 'H2S', 'H3+', 'HCN', 'K', 
-                              'Li', 'LiH', 'MgH', 'N2', 'N2O', 'NH', 'NH3', 
-                              'NO', 'NO2', 'Na', 'NaH', 'O2', 'O3', 'OH', 
-                              'PH3', 'PN', 'PO', 'PS', 'Rb', 'SH', 'SO2', 'ScH', 
-                              'SiH', 'SiO', 'Ti', 'Ti+', 'TiH', 'TiO', 'VO',
-                              'Mg', 'Mg+', 'Ca', 'Ca+', 'Mn' 'Cr', 'V', 'V+',
-                              'Ba', 'Ba+', 'Al', 'Ni', 'O', 'Sc', 'H',
-                              'CH3', 'CS2', 'C2H4', 'C2H6', 'CH3CN', 'CH3OH', 'GeH4'])
+supported_species = np.array(['H2O', 'CO2', 'CH4', 'CO', 'Na', 'K', 'NH3', 'HCN',
+                              'SO2', 'H2S', 'PH3', 'C2H2', 'OCS', 'TiO', 'VO',
+                              'AlO', 'SiO', 'CaO', 'MgO', 'NaO', 'LaO', 'ZrO', 
+                              'SO', 'NO', 'PO', 'TiH', 'CrH', 'FeH', 'ScH',
+                              'AlH', 'SiH', 'BeH', 'CaH', 'MgH', 'LiH', 'NaH',
+                              'OH', 'OH+', 'CH', 'NH', 'SH', 'PN', 'PS', 'CS',
+                              'C2', 'CH3', 'H3+', 'N2O', 'NO2', 'C2H4', 'C2H6', 
+                              'CH3CN', 'CH3OH', 'GeH4', 'CS2', 'O2', 'O3', 'Al', 
+                              'Ba', 'Ba+', 'Ca', 'Ca+', 'Cr', 'Cs', 'Fe', 'Fe+', 
+                              'Li', 'Mg', 'Mg+', 'Mn', 'Ni', 'O', 'Rb', 'Sc', 
+                              'Ti', 'Ti+', 'V', 'V+'])
 
 # Chemical species supported in the fastchem equilibrium grid
 fastchem_supported_species = np.array(['H2O', 'CO2', 'OH', 'SO', 'C2H2', 
@@ -32,7 +33,7 @@ supported_cia = np.array(['H2-H2', 'H2-He', 'H2-H', 'H2-CH4', 'CO2-H2', 'N2-H2',
                           'O2-CO2', 'O2-N2'])
 
 # Species treated as spectrally inactive (in standard cross section treatment)
-inactive_species = np.array(['H2', 'He', 'e-', 'H-'])  # H- handled separately
+inactive_species = np.array(['H2', 'He', 'e-', 'H-', 'N2'])  # H- handled separately
 
 # Aerosol Supported Species
 aerosol_supported_species = np.array(['ADP', 'Al2O3', 'Al2O3_KH', 'C', 'CH4', 'CaTiO3', 'CaTiO3_KH', 'Cr', 'ExoHaze_1000xSolar_300K', 
