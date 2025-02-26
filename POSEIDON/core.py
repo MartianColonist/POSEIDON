Fixed surface parameters to be linear and log, and also so the weighting works in forward models and retrievals.

POSEIDON/core.py
@@ -709,6 +709,7 @@ def define_model(model_name, bulk_species, param_species,
              'surface_temp': surface_temp,
              'surface_model': surface_model,
              'surface_component_albedos': surface_component_albedos,
+             'surface_percentage_option': surface_percentage_option
              }
 
     return model
@@ -1258,6 +1259,7 @@ def compute_spectrum(planet, star, model, atmosphere, opac, wl,
     surface_model = model['surface_model']
     surface_component_albedos = model['surface_component_albedos']
     surface_components = model['surface_components']
+    surface_percentage_option = model['surface_percentage_option']
 
     # Check that the requested spectrum model is supported
     if (spectrum_type not in ['transmission', 'emission', 'direct_emission',
@@ -1326,7 +1328,14 @@ def compute_spectrum(planet, star, model, atmosphere, opac, wl,
     R_p_ref = atmosphere['R_p_ref']
     T_surf = atmosphere['T_surf']
     albedo_surf = atmosphere['albedo_surf']
+
     surface_component_percentages = atmosphere['surface_component_percentages']
+
+    # Normalize the percentages so they add up to one
+    # (note that in forward models, this is necessary but doesn't change input variables)
+    # (in retrievals, they should be normalized before they get to this step so that cube [drawn parameters] is updated)
+    if np.sum(surface_component_percentages) != 1.0:
+        surface_component_percentages = surface_component_percentages/np.sum(surface_component_percentages)
 
     # Check if haze enabled in the cloud model
     if ('haze' in model['cloud_type']):
@@ -1608,7 +1617,7 @@ def compute_spectrum(planet, star, model, atmosphere, opac, wl,
                             surf_reflect = np.full_like(wl, albedo_surf)
 
                         elif surface_model == 'lab_data':
-                            surf_reflect_array = interpolate_surface_components(wl,surface_components,surface_component_albedos,)
+                            surf_reflect_array = interpolate_surface_components(wl,surface_components,surface_component_albedos)
                     
                     else:
                         surf_reflect = np.full_like(wl, albedo_deck)
@@ -2889,8 +2898,8 @@ def set_priors(planet, star, model, data, prior_types = {}, prior_ranges = {}):
             if (prior_types[parameter] == 'CLR_surface'):
 
                 # If the prior is [0,1] stop it immediately 
-                if (prior_ranges[parameter][0] == 0) and (prior_ranges[parameter][1] > 1):
-                    raise Exception('CLR Prior works in log space, even though retrieved values will be from 0 to 1. Set prior like [-12,0].')
+                if (prior_ranges[parameter][0] == 0) and (prior_ranges[parameter][1] > 0):
+                    raise Exception('CLR Prior works in log space. Set surface_percentage_option = \'log\' in define_model() and set prior for all log percentages something like [-12,0].')
 
                 CLR_surface_limit = prior_ranges[parameter][0]
                 

POSEIDON/parameters.py
@@ -2239,16 +2239,30 @@ def unpack_surface_params(param_names, surface_in,
         albedo_surf = 0
     
     if any("percentage" in s for s in surface_param_names):
-        try:
-            surface_component_percentages = surface_in[np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]]
-        except:
-            # In retrievals, surface_in is not an array so the above statement doesn't work
-            surface_in = np.array(surface_in)
-            surface_component_percentages = surface_in[np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]]
-            
-        # Normalize the percentages so they add up to one
-        surface_component_percentages = surface_component_percentages/np.sum(surface_component_percentages)
+        # If they are log percentages, take 10** 
+        if any("log" in s for s in surface_param_names[np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]]):
+
+            try:
+                surface_component_percentages = np.power(10.0,surface_in[np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]])
+            except:
+                # In retrievals, surface_in is not an array so the above statement doesn't work
+                surface_in = np.array(surface_in)
+                surface_component_percentages = np.power(10.0,surface_in[np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]])
+
+        # else, assume they are linear 
+        else:
+            try:
+                surface_component_percentages = surface_in[np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]]
+            except:
+                # In retrievals, surface_in is not an array so the above statement doesn't work
+                surface_in = np.array(surface_in)
+                surface_component_percentages = surface_in[np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]]
+                
     else:
         surface_component_percentages = [1]
+
+    # Note that surface_component_percentages are later to be ensured to add to one in core.py, compute_spectrum
+    # The reason they aren't here is because 
+    # they can be normalized to one before they go to CLR_Surface in retrieval.py which is not good and biases the retrieval
     
     return P_surf, T_surf, albedo_surf, surface_component_percentages

POSEIDON/retrieval.py
@@ -875,8 +875,7 @@ def PyMultiNest_retrieval(planet, star, model, opac, data, prior_types,
                 #print('log_X: ', log_X)
                 #print('10**log_X: ',10**log_X)
                 
-                # Convert back to surface_percentages from log_X
-                surface_percentages_CLR = 10**log_X
+                surface_percentages_CLR = log_X
             else:
                 # For n = 2, you just take the average
                 surface_percentages_CLR = surface_percentage_drawn/np.sum(surface_percentage_drawn)
@@ -884,8 +883,7 @@ def PyMultiNest_retrieval(planet, star, model, opac, data, prior_types,
                 # For the global variable, this must be defined
                 log_X = np.log10(surface_percentages_CLR)
 
-                surface_percentages_CLR = 10**log_X
-
+                surface_percentages_CLR = log_X
 
             # Check if this random parameter draw lies in the allowed simplex space (X_i > 10^-12 and sum to 1)
             global allowed_simplex_surfaces     # Needs a global, as prior function has no return
@@ -902,6 +900,40 @@ def PyMultiNest_retrieval(planet, star, model, opac, data, prior_types,
             counter = 0
             for n in surface_percentage_indices:
                 cube[n] = surface_percentages_CLR[counter]
+                counter += 1
+
+        # If the surface percentages are uniform, need to make sure they are normalized to 1 
+        # Note that this step occurs later in core.py in compute_spectrum()
+        # But it also needs to happen here for retrievals so that cube is updated with correct values
+        if any('percentage' in s for s in surface_param_names ) and ('CLR_surface' not in prior_types.values()):
+
+            # cube is not an array, and has to be turned into an array for the next line
+            # here we are drawing the drawn parameters that correspond to surface params
+            surface_drawn = np.array(cube[N_params_cum[7]:N_params_cum[8]])
+
+            # then we only pull the ones that are specifically for the percentages 
+            surface_percentage_indices = np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]
+            surface_percentage_drawn = surface_drawn[np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]]
+
+            # if they are log, will need to take 10** before normalizing 
+            if any("log" in s for s in surface_param_names[np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]]):
+                surface_percentage_drawn = np.power(10,surface_percentage_drawn)
+
+            # Normalize the percentages so they add up to one 
+            surface_percentages_normalized = surface_percentage_drawn/np.sum(surface_percentage_drawn)
+
+            # If they are log, take the log again after normalizing 
+            if any("log" in s for s in surface_param_names[np.where(np.char.find(surface_param_names,'percentage')!= -1)[0]]):
+                surface_percentages_normalized = np.log10(surface_percentages_normalized)
+
+            # Redefine cube with those percentages 
+            # adds number of params before surface_percentage_indices to get right index in cube
+            surface_percentage_indices = N_params_cum[7]+surface_percentage_indices
+            
+            # I couldn't get the CLR prior to work, but this just replaces things in the cube... which should work
+            counter = 0
+            for n in surface_percentage_indices:
+                cube[n] = surface_percentages_normalized[counter]
                 counter += 1
             
     # Define the log-likelihood function

POSEIDON/visuals.py
@@ -4100,7 +4100,10 @@ def plot_histograms(planet, models, plot_parameters,
                     tick_labelsize = 12, title_fontsize = 12,
                     custom_labels = [], custom_ticks = [],
                     alpha_hist = 0.4,
-                    skip_retrieving_atmospheres = False
+                    skip_retrieving_atmospheres = False,
+                    percent_decimal_to_percent = True,
+                    log_percent_to_percent = False,
+                    percent_to_log_percent = False
                     ):
 
     '''
@@ -4268,13 +4271,7 @@ def plot_histograms(planet, models, plot_parameters,
                         if ('log_' in param): 
                             param_samples_m[:,q] = np.log10(element_ratio_norm)
                         else:
-                            param_samples_m[:,q] = element_ratio_norm
-
-                # Option to make the surface percentages into percents instead of decimals 
-                #elif ('percentage' in param): 
-                #    
-                #    param_samples_m[:,q] = samples[:,np.where(param_names == param)[0][0]] * 100
-                    
+                            param_samples_m[:,q] = element_ratio_norm                        
 
                 # Filler gas
                 else:
@@ -4288,6 +4285,24 @@ def plot_histograms(planet, models, plot_parameters,
                             param_samples_m[:,q] = np.log10(X_stored[:,1])
                         else:
                             param_samples_m[:,q] = np.log10(X_stored[:,0])
+
+                # Option to make log surface percentages into decimal percents 
+                if (log_percent_to_percent == True) and ('percentage' in param):
+
+                    if (percent_decimal_to_percent == True):
+                        param_samples_m[:,q] = np.power(10,samples[:,np.where(param_names == param)[0][0]]) * 100
+                    else:
+                        param_samples_m[:,q] = np.power(10,samples[:,np.where(param_names == param)[0][0]])
+                
+                if (percent_to_log_percent == True) and ('percentage' in param):
+                        param_samples_m[:,q] = np.log10(samples[:,np.where(param_names == param)[0][0]])
+
+                # Option to make the surface percentages into percents instead of decimals 
+                if (percent_decimal_to_percent == True) and ('percentage' in param):
+
+                    # Log is treated above seperatly 
+                    if ('log' not in param):
+                        param_samples_m[:,q] = samples[:,np.where(param_names == param)[0][0]] * 100
 
         param_vals.append(param_samples_m)
 