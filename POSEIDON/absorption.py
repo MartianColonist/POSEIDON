8a169adbd8775c99526d2f6cc01673bde7c52e65
Ryan MacDonald <rmacdonald@astro.cornell.edu>
2025-06-05 19:43:22 +0100

    fix typos

--- POSEIDON/absorption.py
+++ POSEIDON/absorption.py
@@ -1071,16 +1071,16 @@ def extinction(chemical_species, active_species, cia_pairs, ff_pairs, bf_species
     kappa_Ray = np.zeros(shape=(N_layers, N_sectors, N_zones, N_wl))
     kappa_cloud = np.zeros(shape=(N_layers, N_sectors, N_zones, N_wl))
 
-    # kappa_cloud is a total cloud opacity. This array instead splits it up so we can multiple clouds 
+    # kappa_cloud is a total cloud opacity. This array instead splits it up so we can have multiple clouds 
     # in a scattering scenario
 
     # If Mie clouds are turned on
     #if (enable_Mie ==1):
-    #    kappa_cloud_seperate = np.zeros(shape=(len(n_aerosol_array),N_layers, N_sectors, N_zones, N_wl))
+    #    kappa_cloud_separate = np.zeros(shape=(len(n_aerosol_array),N_layers, N_sectors, N_zones, N_wl))
     ## Else its an empty array for numba gods 
     #else:
-    #    kappa_cloud_seperate = np.zeros(shape=(len(n_aerosol_array),N_layers, N_sectors, N_zones, N_wl))
-    kappa_cloud_seperate = np.zeros(shape=(len(n_aerosol_array),N_layers, N_sectors, N_zones, N_wl))
+    #    kappa_cloud_separate = np.zeros(shape=(len(n_aerosol_array),N_layers, N_sectors, N_zones, N_wl))
+    kappa_cloud_separate = np.zeros(shape=(len(n_aerosol_array), N_layers, N_sectors, N_zones, N_wl))
     
     # Fine temperature grid (for pre-interpolating opacities)    
     N_T_fine = len(T_fine)
@@ -1209,22 +1209,22 @@ def extinction(chemical_species, active_species, cia_pairs, ff_pairs, bf_species
                         for i in range(i_bot,N_layers):
                             for q in range(len(wl)):
                                 kappa_cloud[i,j,k,q] += n_aerosol_array[aerosol][i,j,k] * sigma_Mie_array[aerosol][q]
-                                kappa_cloud_seperate[aerosol,i,j,k,q] += n_aerosol_array[aerosol][i,j,k] * sigma_Mie_array[aerosol][q]
+                                kappa_cloud_separate[aerosol,i,j,k,q] += n_aerosol_array[aerosol][i,j,k] * sigma_Mie_array[aerosol][q]
                     
                 # Opaque Deck is the first element in n_aerosol_array
                 else:
                     for aerosol in range(len(n_aerosol_array)):
                         if aerosol == 0:
                             kappa_cloud[(P > P_cloud[0]),j,k,:] += 1.0e250
-                            kappa_cloud_seperate[aerosol,(P > P_cloud[0]),j,k,:] += 1.0e250
+                            kappa_cloud_separate[aerosol,(P > P_cloud[0]),j,k,:] += 1.0e250
                         else:
                             for i in range(i_bot,N_layers):
                                 for q in range(len(wl)):
                                     kappa_cloud[i,j,k,q] += n_aerosol_array[aerosol][i,j,k]* sigma_Mie_array[aerosol-1][q]
-                                    kappa_cloud_seperate[aerosol,i,j,k,q] += n_aerosol_array[aerosol][i,j,k] * sigma_Mie_array[aerosol-1][q]
+                                    kappa_cloud_separate[aerosol,i,j,k,q] += n_aerosol_array[aerosol][i,j,k] * sigma_Mie_array[aerosol-1][q]
           
           
-    return kappa_gas, kappa_Ray, kappa_cloud, kappa_cloud_seperate
+    return kappa_gas, kappa_Ray, kappa_cloud, kappa_cloud_separate
 
 
 @cuda.jit
