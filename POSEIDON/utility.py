c46cc758c30dd7c10fa41ad6b7c70026456a1eb0
Ryan MacDonald <rmacdonald@astro.cornell.edu>
2026-04-06 22:10:53 +0100

    Fix latex string syntax errors for Python 3.12

--- POSEIDON/utility.py
+++ POSEIDON/utility.py
@@ -1202,7 +1202,7 @@ def generate_latex_param_names(param_names):
         if (param in ['Delta_T_1', 'Delta_T_2', 'Delta_T_3', 'Delta_T_4', 
                       'Delta_T_5', 'Delta_T_6', 'Delta_T_7', 'Delta_T_8',
                       'Delta_T_9', 'Delta_T_10', 'Delta_T_11', 'Delta_T_12']):
-            latex_names += ['$\Delta \\, T_{\\mathrm{' + param[8:] + '}}$']
+            latex_names += ['$\\Delta \\, T_{\\mathrm{' + param[8:] + '}}$']
             continue
         if (param == 'T_phot_PT'):
             latex_names += ['$T_{\\mathrm{phot}}$']
@@ -1278,20 +1278,20 @@ def generate_latex_param_names(param_names):
 
         if ('Delta_log_P_' in param):
             if('SiO2' in param):
-                string = '$\\Delta \\, \\log \\, \mathrm{P} \\, \\mathrm{SiO_2}$'
+                string = '$\\Delta \\, \\log \\, \\mathrm{P} \\, \\mathrm{SiO_2}$'
                 latex_names += [string]
                 continue
             if('Fe2O3' in param):
-                string = '$\\Delta \\, \\log \\, \mathrm{P} \\,  \\mathrm{Fe_2O_3}$'
+                string = '$\\Delta \\, \\log \\, \\mathrm{P} \\,  \\mathrm{Fe_2O_3}$'
                 latex_names += [string]
                 continue
             if('MgSiO3' in param):
-                string = '$\\Delta \\, \\log \\, \mathrm{P} \\,  \\mathrm{MgSiO_3}$'
+                string = '$\\Delta \\, \\log \\, \\mathrm{P} \\,  \\mathrm{MgSiO_3}$'
                 latex_names += [string]
                 continue
             else:
                 aerosol_name = param.split('_')[3]
-                string = '$\\Delta \\, \\log \\, \mathrm{P} \\, \\mathrm{' + aerosol_name + '}$'
+                string = '$\\Delta \\, \\log \\, \\mathrm{P} \\, \\mathrm{' + aerosol_name + '}$'
                 latex_names += [string]
                 continue
 
@@ -1329,7 +1329,7 @@ def generate_latex_param_names(param_names):
         #  Quick fix for cloud_type = 'one_slab'
         # 'Delta_log_P_' will not be recognised so new if statement can be made
         if ('Delta_log_P' == param):
-            string = '$\\Delta \\, \\log \\, \mathrm{P}$'
+            string = '$\\Delta \\, \\log \\, \\mathrm{P}$'
             latex_names += [string]
             continue
 
--- POSEIDON/visuals.py
+++ POSEIDON/visuals.py
@@ -4138,7 +4138,7 @@ def plot_PT_retrieved(planet_name, PT_median, PT_low2, PT_low1, PT_high1,
 
         if log_P_surf_sigma_upper_lower == 'upper':
             #ax1.axhspan(P_surf_low,np.max(P), lw = 0.0, alpha = 0.5, color = 'darkgray')
-            ax1.axhline(P_surf_low, lw = 3.0, color = scale_lightness(colours[i], 1.0), label = 'Surface Pressure ($1 \sigma$ upper)')
+            ax1.axhline(P_surf_low, lw = 3.0, color = scale_lightness(colours[i], 1.0), label = r'Surface Pressure ($1 \sigma$ upper)')
             ax1.axhspan(P_surf_low,median_P_surf, lw = 0.0, alpha = 0.5, color = colours[i],)
             ax1.axhspan(median_P_surf,P_surf_high, lw = 0.0, alpha = 0.25, color = colours[i])
             #ax1.axhspan(P_surf_low,np.max(P), lw = 0.0, alpha = 0.5, color = 'darkgray')
@@ -4151,7 +4151,7 @@ def plot_PT_retrieved(planet_name, PT_median, PT_low2, PT_low1, PT_high1,
 
         elif log_P_surf_sigma_upper_lower == 'lower':
             ax1.axhspan(median_P_surf,np.max(P), lw = 0.0, alpha = 0.5, color = 'darkgray')
-            ax1.axhline(P_surf_high, lw = 3.0, color = scale_lightness(colours[i], 1.0), label = 'Surface Pressure ($1 \sigma$ lower)')
+            ax1.axhline(P_surf_high, lw = 3.0, color = scale_lightness(colours[i], 1.0), label = r'Surface Pressure ($1 \sigma$ lower)')
             ax1.axhspan(P_surf_high,median_P_surf, lw = 0.0, alpha = 0.5, color = colours[i],)
             ax1.axhspan(median_P_surf,P_surf_low, lw = 0.0, alpha = 0.25, color = colours[i],)
             ax1.axhspan(P_surf_high,np.max(P), lw = 0.0, alpha = 0.5, color = 'darkgray',)
@@ -4204,7 +4204,7 @@ def plot_PT_retrieved(planet_name, PT_median, PT_low2, PT_low1, PT_high1,
             ax_histy.set_xticks([])
             ax_histy.yaxis.set_label_position("right")
             if log_P_surf_title == True:
-                ax_histy.set_ylabel('log P$_{\mathrm{surf}}$', rotation = 270, labelpad = 20, fontsize = 15)
+                ax_histy.set_ylabel(r'log P$_{\mathrm{surf}}$', rotation = 270, labelpad = 20, fontsize = 15)
 
 
 
